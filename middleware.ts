224c02d36b7b9f92e814968e016b735613a7fbfc
Claudio Martinez <clamart85hs@gmail.com>
2024-12-04 09:59:50 +0100

    fix middle

--- middleware.ts
+++ middleware.ts
@@ -12,11 +12,10 @@ export function middleware(req: NextRequest) {
 
   const apiKey = apiKeyHeader || apiKeyQuery; 
 
-  if (!apiKey || apiKey !== process.env.NEXT_PUBLIC_API_KEY) {
+  if (!apiKey || apiKey !== process.env.API_KEY) {
     return NextResponse.json(
       { error: "Non autorizzato: API Key non valida" },
-      { status: 401 }
-      
+      { status: 401 }      
     );
   }
 
